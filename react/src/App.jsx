a1506cb31610b644014f64b104e5be9d01d23297
sohrab <sohrab.gh727272@gmail.com>
2023-06-28 18:28:00 +0330

    install heroIcon

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import {UserIcon} from "@heroicons/react/24/solid";
 
 function App() {
 
   return (
       <div className="bg-purple-500">
        App.jsx
+          <UserIcon />
       </div>
   )
 }
