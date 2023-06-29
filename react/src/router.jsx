0b3c760a524829eb83bdf3ed3d33a7db613810f6
sohrab <sohrab.gh727272@gmail.com>
2023-06-30 01:18:48 +0330

    add components

---
+++ react/src/router.jsx
@@ -0,0 +1,23 @@
+import {createBrowserRouter} from "react-router-dom";
+import Dashboard from './views/Dashboard.jsx';
+import Surveys from './views/Surveys.jsx';
+import Login from './views/Login.jsx';
+import SignUp from './views/SignUp.jsx';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Dashboard />
+    },{
+        path:'/surveys',
+        element:<Surveys />
+    },{
+        path:'/login',
+        element:<Login />
+    },{
+        path:'/signup',
+        element:<SignUp />
+    },
+]);
+
+export default router;
--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,4 +1,4 @@
-export default function App() {
+export default function Login() {
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
