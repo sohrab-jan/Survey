a1a149a1dbaef601f605bb5b6cfbef0191895c61
sohrab <sohrab.gh727272@gmail.com>
2023-06-30 17:24:17 +0330

    add login and signup into GuestLayout

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,22 +1,32 @@
-import {createBrowserRouter} from "react-router-dom";
-import Dashboard from './views/Dashboard.jsx';
-import Surveys from './views/Surveys.jsx';
-import Login from './views/Login.jsx';
-import SignUp from './views/SignUp.jsx';
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "./views/Dashboard.jsx";
+import Surveys from "./views/Surveys.jsx";
+import Login from "./views/Login.jsx";
+import SignUp from "./views/SignUp.jsx";
+import GuestLayout from "./components/GuestLayout";
 
 const router = createBrowserRouter([
     {
-        path:'/',
-        element:<Dashboard />
-    },{
-        path:'/surveys',
-        element:<Surveys />
-    },{
-        path:'/login',
-        element:<Login />
-    },{
-        path:'/signup',
-        element:<SignUp />
+        path: "/",
+        element: <Dashboard />,
+    },
+    {
+        path: "/surveys",
+        element: <Surveys />,
+    },
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <SignUp />,
+            },
+        ],
     },
 ]);
 
--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -2,21 +2,16 @@ export default function SignUp() {
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                    />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign up for free
-                    </h2>
-                </div>
-
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign up for free
+                </h2>
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" action="#" method="POST">
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                            >
                                 Email address
                             </label>
                             <div className="mt-2">
@@ -33,11 +28,17 @@ export default function SignUp() {
 
                         <div>
                             <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                <label
+                                    htmlFor="password"
+                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                >
                                     Password
                                 </label>
                                 <div className="text-sm">
-                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                    <a
+                                        href="#"
+                                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                                    >
                                         Forgot password?
                                     </a>
                                 </div>
@@ -65,13 +66,16 @@ export default function SignUp() {
                     </form>
 
                     <p className="mt-10 text-center text-sm text-gray-500">
-                        Not a member?{' '}
-                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                        Not a member?{" "}
+                        <a
+                            href="#"
+                            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                        >
                             Start a 14 day free trial
                         </a>
                     </p>
                 </div>
             </div>
         </>
-    )
+    );
 }
