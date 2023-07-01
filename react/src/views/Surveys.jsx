f954f5921fc01b60fe69512884682b7fd4c701bd
sohrab <sohrab.gh727272@gmail.com>
2023-07-01 10:45:32 +0330

    add PageComponent.jsx

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,18 +1,9 @@
-export default function Surveys() {
+import PageComponent from '../components/PageComponent';
+
+export default function Dashboard() {
     return (
-        <>
-            <header className="bg-white shadow">
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                        Surveys
-                    </h1>
-                </div>
-            </header>
-            <main>
-                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                    {/* Your content */}
-                </div>
-            </main>
-        </>
+        <PageComponent title="Surveys">
+            Surveys Title
+        </PageComponent>
     );
 }
