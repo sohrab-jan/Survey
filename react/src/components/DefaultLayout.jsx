f57fd8ea3c8b10fc6224882b8647189bfb658342
sohrab <sohrab.gh727272@gmail.com>
2023-07-01 14:36:45 +0330

    add ContextProvider.jsx for handling auth

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -2,18 +2,14 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { NavLink, Outlet } from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider";
+import {UserIcon} from "@heroicons/react/24/solid";
+import {Navigate } from "react-router-dom";
 
-const user = {
-    name: "Tom Cook",
-    email: "tom@example.com",
-    imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
     { name: "Dashboard", to: "/" },
     { name: "Survey", to: "/surveys" },
 ];
-const userNavigation = [{ name: "Sign out", href: "#" }];
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -25,6 +21,11 @@ const logout = (ev) => {
     console.log("logout");
 };
 export default function DefaultLayout() {
+    const {currentUser ,userToken} = useStateContext();
+    if (!userToken){
+        return <Navigate to='/login' />
+    }
+
     return (
         <>
             <div className="min-h-full">
@@ -76,11 +77,7 @@ export default function DefaultLayout() {
                                                         <span className="sr-only">
                                                             Open user menu
                                                         </span>
-                                                        <img
-                                                            className="h-8 w-8 rounded-full"
-                                                            src={user.imageUrl}
-                                                            alt=""
-                                                        />
+                                                        <UserIcon className="w-8 h-8 p-2 bg-black/25 text-white rounded-full"/>
                                                     </Menu.Button>
                                                 </div>
                                                 <Transition
@@ -161,18 +158,14 @@ export default function DefaultLayout() {
                                 <div className="border-t border-gray-700 pb-3 pt-4">
                                     <div className="flex items-center px-5">
                                         <div className="flex-shrink-0">
-                                            <img
-                                                className="h-10 w-10 rounded-full"
-                                                src={user.imageUrl}
-                                                alt=""
-                                            />
+                                            <UserIcon className="w-8 h-8 p-2 bg-black/25 text-white rounded-full"/>
                                         </div>
                                         <div className="ml-3">
                                             <div className="text-base font-medium leading-none text-white">
-                                                {user.name}
+                                                {currentUser.name}
                                             </div>
                                             <div className="text-sm font-medium leading-none text-gray-400">
-                                                {user.email}
+                                                {currentUser.email}
                                             </div>
                                         </div>
                                     </div>
