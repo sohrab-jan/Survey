adad2a6d1709c7a61dca8a01c4ee1786bde071c8
sohrab <sohrab.gh727272@gmail.com>
2023-07-02 15:05:49 +0330

    add surveys lists

---
+++ react/src/components/SurveysListItem.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export  default function SurveysListItem(survey){
+    return (<div>SurveysListItem</div>)
+}
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,9 +1,16 @@
 import PageComponent from '../components/PageComponent';
+import SurveysListItem from '../components/SurveysListItem';
+import {useStateContext} from "../contexts/ContextProvider";
 
-export default function Dashboard() {
+export default function Surveys() {
+    const {surveys} =useStateContext();
     return (
         <PageComponent title="Surveys">
-            Surveys Title
+            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3" >
+                {surveys.map(survey => (
+                    <SurveysListItem survey={survey}  key={survey.id}/>
+                ))}
+            </div>
         </PageComponent>
     );
 }
