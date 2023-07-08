e4cfc83f32ab209044f7148b3e29fa851f8081f9
sohrab <sohrab.gh727272@gmail.com>
2023-07-09 00:19:22 +0330

    refactor SignUpRequest.php and LoginRequest.php

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -9,7 +9,9 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-
+            'email' => ['required','email','string','unique:users,email'],
+            'password' => ['required'],
+            'remember' => ['boolean'],
         ];
     }
 
--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -3,13 +3,16 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class SignUpRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-
+            'name' => ['required','string'],
+            'email' => ['required','email','string','unique:users,email'],
+            'password' => ['required','confirmed', Password::min(8)->mixedCase()->numbers()->symbols()],
         ];
     }
 
