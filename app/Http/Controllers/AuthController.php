b3c4e01c01a9da3f7a658309acdd971fc03b3066
sohrab <sohrab.gh727272@gmail.com>
2023-07-08 22:29:13 +0330

    add AuthController.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignUpRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(SignUpRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (! Auth::attempt($credentials, $remember)) {
+            return response(['error' => 'invalid username or password'], 422);
+        }
+        $user = Auth::user();
+        $token = $user?->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ]);
+
+    }
+
+    public function logout()
+    {
+        /** @var User $user */
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
+
+        return response([
+            'success' => true,
+        ]);
+
+    }
+}
