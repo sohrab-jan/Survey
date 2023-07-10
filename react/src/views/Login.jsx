08c09a2da88358f93b9441c590cc3a987117efa4
sohrab <sohrab.gh727272@gmail.com>
2023-07-10 13:08:57 +0330

    finish login

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,58 +1,102 @@
+import { useState } from "react";
+import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios";
+import { Link } from "react-router-dom";
+
 export default function Login() {
+    const {setCurrentUser,setUserToken} = useStateContext();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState({__html:''});
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        setError({ __html: "" });
+
+        axiosClient
+            .post("/login", {
+                email,
+                password,
+            })
+            .then(({ data }) => {
+                setCurrentUser(data.user);
+                setUserToken(data.token);
+            })
+            .catch((error) => {
+                if (error.response) {
+                    const finalErrors = Object.values(
+                        error.response.data.errors
+                    ).reduce((accum, next) => [...accum, ...next], []);
+                    setError({ __html: finalErrors.join("<br>") });
+                }
+                console.error(error);
+            });
+    };
+
     return (
         <>
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
-            </h2>
+                    Sign in to your account
+                </h2>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                    Or{" "}
+                    <Link
+                        to="/signup"
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                        Sign up for free
+                    </Link>
+                </p>
+            {/*\\TODO fix displaying error messages*/}
+            {error.__html && (
+                    <div
+                        className="bg-red-500 rounded py-2 px-3 text-white"
+                        dangerouslySetInnerHTML={error}
+                    ></div>
+                )}
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" action="#" method="POST">
-                    <div>
-                        <label
-                            htmlFor="email"
-                            className="block text-sm font-medium leading-6 text-gray-900"
-                        >
-                            Email address
-                        </label>
-                        <div className="mt-2">
-                            <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <label
-                                htmlFor="password"
-                                className="block text-sm font-medium leading-6 text-gray-900"
-                            >
-                                Password
+                <form onSubmit={onSubmit} className="space-y-6" action="#" method="POST">
+                <div>
+                            <label htmlFor="email" className="sr-only">
+                                Email
                             </label>
-                            <div className="text-sm">
-                                <a
-                                    href="#"
-                                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                >
-                                    Forgot password?
-                                </a>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    value={email}
+                                    onChange={(ev) => setEmail(ev.target.value)}
+                                    placeholder="Email Address"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
+                                    ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
+                                     focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
                             </div>
                         </div>
-                        <div className="mt-2">
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                autoComplete="current-password"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
+                        <div>
+                            <label htmlFor="password" className="sr-only">
+                                Password
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    value={password}
+                                    onChange={(ev) =>
+                                        setPassword(ev.target.value)
+                                    }
+                                    placeholder="Password"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
+                                     ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
+                                      focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
                         </div>
-                    </div>
 
                     <div>
                         <button
