c34e139333b6a15e3a812f7f41b1ebd8f20f4a2a
sohrab <sohrab.gh727272@gmail.com>
2023-07-17 12:54:50 +0330

    add QuestionEditor.jsx

--- react/src/components/QuestionEditor.jsx
+++ react/src/components/QuestionEditor.jsx
@@ -26,15 +26,20 @@ export default function QuestionEditor({
                     {index + 1} . {model.question}
                 </h4>
                 <div className="flex items-center">
-                    <button type="button" className="flex items-center text-xs py-1 px-3 mr-2
+                    <button
+                        type="button"
+                        className="flex items-center text-xs py-1 px-3 mr-2
                      rounded-sm text-white bg-gray-600 hover:bg-gray-700"
                         onClick={addQuestion}>
                         <PlusIcon className="w-4" />
                         Add
                     </button>
-                    <button type="button" className="flex items-center text-xs py-1 px-3 mr-2
-                     rounded-sm text-red-500 bg-red-500 hover:bg-red-600"
-                        onClick={deleteQuestion(question)}>
+                    <button
+                        type="button"
+                        className="flex items-center text-xs py-1 px-3 mr-2
+                     rounded-sm text-red-500 bg-red-500 hover:bg-red-600 font-semibold"
+                    // onClick={deleteQuestion(question)}
+                    >
                         <TrashIcon className="w-4" />
                         Delete
                     </button>
@@ -50,12 +55,58 @@ export default function QuestionEditor({
                         type="text"
                         name="question"
                         id="question"
+                        value={model.question}
+                        onChange={(ev) => setModel({ ...model, question: ev.target.value })}
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 
                        focus:ring-indigo-500 sm:text-sm"
                     />
                 </div>
                 {/* Question Text */}
 
+                {/* Question Type */}
+                <div>
+                    <label
+                        htmlFor="questionType"
+                        className="block text-sm font-medium text-gray-700 w-40"
+                    >
+                        Question Type
+                    </label>
+                    <select
+                        name="questionType"
+                        id="questionType"
+                        onChange={(ev) => setModel({ ...model, type: ev.target.value })}
+                        className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm 
+                        focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                        {questionTypes.map((type) => (
+                            <option value={type} selected={model.type == type}>
+                                {upperCaseFirst(type)}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                {/* Question Type */}
+
+                {/* Description */}
+                <div>
+                    <label
+                        htmlFor="questionDescription"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Description
+                    </label>
+                    <textarea
+                        name="questionDescription"
+                        id="questionDescription"
+                        value={model.description}
+                        onChange={(ev) =>
+                            setModel({ ...model, description: ev.target.value })
+                        }
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm 
+                        focus:border-indigo-500  focus:ring-indigo-500 sm:text-sm"
+                    ></textarea>
+                </div>
+                {/* Description */}
             </div>
         </div>
     )
--- react/src/components/SurveyQuestions.jsx
+++ react/src/components/SurveyQuestions.jsx
@@ -1,6 +1,7 @@
 import { PlusIcon } from "@heroicons/react/24/outline";
 import QuestionEditor from "./QuestionEditor";
 import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function SurveyQuestions({ survey, onSurveyUpdate }) {
     const [model, setModel] = useState({ ...survey });
@@ -12,7 +13,7 @@ export default function SurveyQuestions({ survey, onSurveyUpdate }) {
                 {
                     id: uuidv4(),
                     type: "text",
-                    questions: "",
+                    question: "",
                     description: "",
                     data: {},
                 },
@@ -34,10 +35,10 @@ export default function SurveyQuestions({ survey, onSurveyUpdate }) {
 
     }
     const deleteQuestion = (question) => {
-        const newQuestion = model.question.filter((q) => q.id !== question.id);
+        const newQuestions = model.questions.filter((q) => q.id !== question.id);
         setModel({
             ...model,
-            questions: newQuestion
+            questions: newQuestions
         });
     };
 
@@ -49,8 +50,12 @@ export default function SurveyQuestions({ survey, onSurveyUpdate }) {
         <>
             <div className="flex justify-between">
                 <h3 className="text-2x1 font-bold">Questions</h3>
-                <button type="button" className="flex items-center text-sm py-1 rounded-sm text-white
-                 bg-gray-600 hover:bg-gray700" onClick={addQuestion}>
+                <button
+                    type="button"
+                    className="flex items-center text-sm py-1 rounded-sm text-white
+                 bg-gray-600 hover:bg-gray700"
+                    onClick={addQuestion}
+                >
                     <PlusIcon className="w-4 mr-2" />
                     Add Question
                 </button>
@@ -58,7 +63,7 @@ export default function SurveyQuestions({ survey, onSurveyUpdate }) {
             {model.questions.length ? (
                 model.questions.map((q, ind) => (
                     <QuestionEditor
-                        key={q}
+                        key={q.id}
                         index={ind}
                         questions={q}
                         questionChange={questionChange}
