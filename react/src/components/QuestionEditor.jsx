353568127cb9d8e7ddec7edb6d70fad096e82a06
sohrab <sohrab.gh727272@gmail.com>
2023-08-21 18:11:43 +0330

    implement create options

--- react/src/components/QuestionEditor.jsx
+++ react/src/components/QuestionEditor.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useStateContext } from "../contexts/ContextProvider";
 import { PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { v4 as uuidv4 } from "uuid";
 
 export default function QuestionEditor({
     index = 0,
@@ -18,104 +19,185 @@ export default function QuestionEditor({
     function upperCaseFirst(str) {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
+    function shouldHaveOptions(type = null) {
+        type = type || model.type;
+        return ['select', 'radio', 'checkbox'].includes(type);
+    }
+    function onTypeChange(ev) {
+        const newModel = {
+            ...model,
+            type: ev.target.value
+        }
+        if (!shouldHaveOptions(model.type) && shouldHaveOptions(ev.target.value)) {
+            if (!model.data.options) {
+                newModel.data = {
+                    options: [
+                        { uuid: uuidv4(), text: '' }
+                    ]
+                }
+            }
+        }
+        setModel(newModel);
+    }
 
+    function addOption() {
+        model.data.options.push({
+            uuid: uuidv4(),
+            text: ''
+        })
+        setModel({ ...model })
+    }
+
+    function deleteOption(op) {
+        model.data.options = model.data.options.filter(option => option.uuid != op.uuid)
+        setModel({ ...model })
+    }
     return (
-        <div>
-            <div className="flex justify-between mb-3">
-                <h4>
-                    {index + 1} . {model.question}
-                </h4>
-                <div className="flex items-center">
-                    <button
-                        type="button"
-                        className="flex items-center text-xs py-1 px-3 mr-2
+        <>
+            <div>
+                <div className="flex justify-between mb-3">
+                    <h4>
+                        {index + 1} . {model.question}
+                    </h4>
+                    <div className="flex items-center">
+                        <button
+                            type="button"
+                            className="flex items-center text-xs py-1 px-3 mr-2
                      rounded-sm text-white bg-gray-600 hover:bg-gray-700"
-                        onClick={() => addQuestion(index)}
-                    >
-                        <PlusIcon className="w-4" />
-                        Add
-                    </button>
-                    <button
-                        type="button"
-                        className="flex items-center text-xs py-1 px-3 mr-2
-                     rounded-sm text-red-500 bg-red-500 hover:bg-red-600 font-semibold"
-                    // onClick={deleteQuestion(question)}//TODO fix delete question
-                    >
-                        <TrashIcon className="w-4" />
-                        Delete
-                    </button>
+                            onClick={() => addQuestion(index + 1)}
+                        >
+                            <PlusIcon className="w-4" />
+                            Add
+                        </button>
+                        <button
+                            type="button"
+                            className="flex items-center text-xs py-1 px-3 mr-2
+                     rounded-sm border border-transparent text-red-500 text-red-500 hover:bg-red-600 font-semibold"
+                            onClick={() => deleteQuestion(question)}//TODO fix delete question
+                        >
+                            <TrashIcon className="w-4" />
+                            Delete
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className="flex gap-3 justify-between mb-3">
-                {/* Question Text */}
-                <div className="flex-1">
-                    <label
-                        htmlFor="question"
-                        className="block text-sm font-medium text-gray-700"
-                    >
-                        Question
-                    </label>
-                    <input
-                        type="text"
-                        name="question"
-                        id="question"
-                        value={model.question}
-                        onChange={(ev) =>
-                            setModel({ ...model, question: ev.target.value })
-                        }
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500
+                <div className="flex gap-3 justify-between mb-3">
+                    {/* Question Text */}
+                    <div className="flex-1">
+                        <label
+                            htmlFor="question"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Question
+                        </label>
+                        <input
+                            type="text"
+                            name="question"
+                            id="question"
+                            value={model.question}
+                            onChange={(ev) =>
+                                setModel({ ...model, question: ev.target.value })
+                            }
+                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500
                        focus:ring-indigo-500 sm:text-sm"
-                    />
-                </div>
-                {/* Question Text */}
+                        />
+                    </div>
+                    {/* Question Text */}
 
-                {/* Question Type */}
-                <div>
-                    <label
-                        htmlFor="questionType"
-                        className="block text-sm font-medium text-gray-700 w-40"
-                    >
-                        Question Type
-                    </label>
-                    <select
-                        name="questionType"
-                        id="questionType"
-                        onChange={(ev) =>
-                            setModel({ ...model, type: ev.target.value })
-                        }
-                        className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm
+                    {/* Question Type */}
+                    <div>
+                        <label
+                            htmlFor="questionType"
+                            className="block text-sm font-medium text-gray-700 w-40"
+                        >
+                            Question Type
+                        </label>
+                        <select
+                            name="questionType"
+                            id="questionType"
+                            onChange={onTypeChange}
+                            value={model.type}
+                            className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm
                         focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    >
-                        {questionTypes.map((type) => (
-                            <option value={type} key={type}>
-                                {upperCaseFirst(type)}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                {/* Question Type */}
+                        >
+                            {questionTypes.map((type) => (
+                                <option value={type} key={type}>
+                                    {upperCaseFirst(type)}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    {/* Question Type */}
 
-                {/* Description */}
-                <div>
-                    <label
-                        htmlFor="questionDescription"
-                        className="block text-sm font-medium text-gray-700"
-                    >
-                        Description
-                    </label>
-                    <textarea
-                        name="questionDescription"
-                        id="questionDescription"
-                        value={model.description || ''}
-                        onChange={(ev) =>
-                            setModel({ ...model, description: ev.target.value })
-                        }
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm
+                    {/* Description */}
+                    <div className="mb-3">
+                        <label
+                            htmlFor="questionDescription"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Description
+                        </label>
+                        <textarea
+                            name="questionDescription"
+                            id="questionDescription"
+                            value={model.description || ''}
+                            onChange={(ev) =>
+                                setModel({ ...model, description: ev.target.value })
+                            }
+                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm
                         focus:border-indigo-500  focus:ring-indigo-500 sm:text-sm"
-                    ></textarea>
+                        ></textarea>
+                    </div>
+                    {/* Description */}
+
+                    {shouldHaveOptions() && <div>
+                        <div>
+                            <h4 className="text-sm font-semibold mb-1 flex justify-between items-center">
+                                Options
+                                <button onClick={addOption} type="button" className="flex items-center text-xs py-1 px-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700">
+                                    Add
+                                </button>
+                            </h4>
+                            {
+                                model.data.options && model.data.options.length === 0 && (
+                                    <div className="text-xs text-gray-600 text-center py-3">
+                                        You don't have any options defined.
+                                    </div>
+                                )
+                            }
+                            {
+                                model.data.options && model.data.options.length > 0 && (
+                                    <div className="flex items-center mb-1">
+                                        {model.data.options.map((op, ind) => (
+                                            <div key={op.uuid} className="flex items-center mb-1">
+                                                <span className="w-6 text-sm">{ind + 1}.</span>
+                                                <input
+                                                    type="text"
+                                                    value={op.text}
+                                                    onInput={ev => { op.text = ev.target.value; setModel({ ...model }) }}
+                                                    className="w-full rounded-sm py-1 px-2 text-xs border 
+                                            border-gray-300 focus:border-indigo-500" />
+                                                <button
+                                                    onClick={ev => deleteOption(op)}
+                                                    type="submit"
+                                                    className="h-6 w-6 rounded-full flex items-center justify-center border border-transparent
+                                             transition-colors hover:border-red-100"
+                                                >
+                                                    <TrashIcon className="w-3 h-3 text-red-500" />
+                                                </button>
+                                            </div>
+                                        ))}
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>}
                 </div>
-                {/* Description */}
+                {
+                    model.type === 'select' && <div>
+
+                    </div>
+                }
             </div>
-        </div>
+            <hr /></>
     );
 }
